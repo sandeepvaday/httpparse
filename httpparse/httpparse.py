04a45c48484d77e0969d7732da40c03fc743da2d
Sandeep Yadav <syadav@netskope.com>
2018-01-01 16:00:33 -0800

    Fix documentation; Move user fn

--- httpparse/httpparse.py
+++ httpparse/httpparse.py
@@ -25,71 +25,6 @@ def user_fn(sess_id, http_req_rsp):
     return
 
 
-def filter_fn(pkt):
-    """Filtering condition for relevant packets wanted from the PCAP. Since
-    we only want HTTP based TCP packets
-
-    Args:
-        pkt (scapy.layers.l2.Ether)     : Each on which to apply the filter
-
-    Returns:
-        bool: True if the pkt met the filter condition. Else, False.
-    """
-    return ((TCP in pkt) and ((pkt[TCP].sport == 80) or (pkt[TCP].dport == 80)))
-
-
-def parse_pcap(pcapfile):
-    """Collect request and response HTTP sessions for a tuple and invoke
-    function to extract relevant meta data.
-
-    Args:
-        userid (str)        : User ID
-        pcapfilename (str)  : Name of the PCAP file to build the sessions
-                              from.
-
-    Returns:
-        list: List of docs with same format as extracted from Page events
-    """
-    pinf = rdpcap(pcapfile)
-    sessions = pinf.filter(filter_fn).sessions()
-
-    sessions_cache = {}
-    for sessid, pkts in sessions.iteritems():
-
-        http_payload = ''
-        for pkt in pkts:
-            # Pretty important: pkt may contain Padding which messes up the
-            # built HTTP content
-            if type(pkt[TCP].payload) != scapy.packet.Raw:
-                continue
-            http_payload += str(pkt[TCP].payload)
-        pet = int(pkts[-1].time)     # Page end time
-
-        # Determine type: Request/Response
-        if pkts[0].dport == 80:
-            # Check if the flow tuple is present in the cache
-            if sessid not in sessions_cache:
-                sessions_cache[sessid] = {}
-            # HTTP request
-            sessions_cache[sessid]['req'] = (http_payload, pet)
-        else:
-            # HTTP response
-            proto, src, _, dst = sessid.split(' ')
-            req_sessid = '%s %s > %s' % (proto, dst, src)
-            # Check if the flow tuple is present in the cache
-            if req_sessid not in sessions_cache:
-                sessions_cache[req_sessid] = {}
-            sessions_cache[req_sessid]['rsp'] = (http_payload, pet)
-
-    for sess_id, v in sessions_cache.iteritems():
-        # Check if HTTP payload is not empty
-        if ('rsp' in v) and (v['rsp'][0] != ''):
-            # Replace with user_fn(sess_id, v)
-            user_fn_example(sess_id, v)
-
-    return
-
-
 def user_fn_example(sess_id, http_req_rsp):
     """Description
 
@@ -138,6 +73,68 @@ def user_fn_example(sess_id, http_req_rsp):
         # Content type
         if field.startswith('content-type'):
             print 'Content-type returned: %s' % content
+
+    return
+
+
+def filter_fn(pkt):
+    """Filtering condition for relevant packets wanted from the PCAP. Since
+    we only want HTTP based TCP packets
+
+    Args:
+        pkt (scapy.layers.l2.Ether)     : Each on which to apply the filter
+
+    Returns:
+        bool: True if the pkt met the filter condition. Else, False.
+    """
+    return ((TCP in pkt) and ((pkt[TCP].sport == 80) or (pkt[TCP].dport == 80)))
+
+
+def parse_pcap(pcapfile):
+    """Collect request and response HTTP sessions.
+
+    Args:
+        pcapfile (str)  : Name of the PCAP file to build the sessions from.
+
+    Returns:
+        Nothing
+    """
+    pinf = rdpcap(pcapfile)
+    sessions = pinf.filter(filter_fn).sessions()
+
+    sessions_cache = {}
+    for sessid, pkts in sessions.iteritems():
+
+        http_payload = ''
+        for pkt in pkts:
+            # Pretty important: pkt may contain Padding which messes up the
+            # built HTTP content
+            if type(pkt[TCP].payload) != scapy.packet.Raw:
+                continue
+            http_payload += str(pkt[TCP].payload)
+        pet = int(pkts[-1].time)     # Page end time
+
+        # Determine type: Request/Response
+        if pkts[0].dport == 80:
+            # Check if the flow tuple is present in the cache
+            if sessid not in sessions_cache:
+                sessions_cache[sessid] = {}
+            # HTTP request
+            sessions_cache[sessid]['req'] = (http_payload, pet)
+        else:
+            # HTTP response
+            proto, src, _, dst = sessid.split(' ')
+            req_sessid = '%s %s > %s' % (proto, dst, src)
+            # Check if the flow tuple is present in the cache
+            if req_sessid not in sessions_cache:
+                sessions_cache[req_sessid] = {}
+            sessions_cache[req_sessid]['rsp'] = (http_payload, pet)
+
+    for sess_id, v in sessions_cache.iteritems():
+        # Check if HTTP payload is not empty
+        if ('rsp' in v) and (v['rsp'][0] != ''):
+            # Replace with user_fn(sess_id, v)
+            user_fn_example(sess_id, v)
 
     return
 
